38a5b9d1d3d9c552ab1814ce146f6840327a7771
Mike Lasby <mklasby@gmail.com>
2025-11-09 11:28:14 -0700

    add util for downloading datasets to cache

--- evalplus/utils.py
+++ evalplus/utils.py
@@ -15,3 +15,13 @@ def progress(note: str = "processing"):
         TextColumn("•"),
         TimeElapsedColumn(),
     )
+
+def download_all_datasets():
+    from evalplus.data.humaneval import get_human_eval_plus
+    from evalplus.data.mbpp import get_mbpp_plus
+
+    get_human_eval_plus()
+    get_mbpp_plus()    
+
+if __name__ == "__main__":
+    download_all_datasets()
